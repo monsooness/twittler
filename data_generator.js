dab9399d634109cdbf44a01aa5c973e2a268ea41
Monsoon <nepzsoon@gmail.com>
2018-05-22 15:12:12 -0700

    Added live update, update feed

--- data_generator.js
+++ data_generator.js
@@ -46,9 +46,9 @@ var generateRandomTweet = function(){
   addTweet(tweet);
 };
 
-for(var i = 0; i < 10; i++){
-  generateRandomTweet();
-}
+// for(var i = 0; i < 10; i++){
+//   generateRandomTweet();
+// }
 
 var scheduleNextTweet = function(){
   generateRandomTweet();
